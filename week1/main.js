315a25292e326ce049d02a53635d4589f2c4fecc
jainam-b <jainambagrecha16@gmail.com>
2023-12-08 22:07:13 +0530

    aray,functions,forloop

---
+++ week1/main.js
@@ -0,0 +1,26 @@
+// let firstname="jainam bagrecha ";
+// let age="19";
+// console.log("this person name is "+firstname+"and age is "+age );
+
+// if(age>18){
+//     console.log("he is major");
+// }
+// else{
+//     console.log ("he is minor");
+// }
+
+// for loop
+// let answer=0;
+// for(let i =0;i<100;i++){
+//     answer=answer+i;
+// }
+// console.log(answer);
+
+// print even number from array
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+for(let i=0;i<=arr.length;i++){
+    if(arr[i]%2==0){
+        console.log(arr[i])
+    }
+}

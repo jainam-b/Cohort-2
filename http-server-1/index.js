8b96dfc529d6bbbc9b400c9e946487c46d56efc1
jainam-b <jainambagrecha16@gmail.com>
2024-01-14 10:05:13 +0530

    endpoint added

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -6,6 +6,9 @@ const port = 3000
 app.get('/', function(req, res)   {
   res.send('Hello World!')
 })
+app.get('/conversation-api', function(req, res)   {
+  res.send('Hello World!')
+})
 
 app.listen(port, function()   {
   console.log(`Example app listening on port ${port}`)

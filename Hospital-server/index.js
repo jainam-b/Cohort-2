6720d5bfdebbb476c49511f1092813adb405bd9b
jainam-b <jainambagrecha16@gmail.com>
2024-01-19 22:26:05 +0530

    hospital game server donegit add .

--- Hospital-server/index.js
+++ Hospital-server/index.js
@@ -7,12 +7,12 @@ const users=[{
         healthy:false
     }]
 }]
-
+app.use(express.json());
 app.get("/",function(req,res){
     const johnKidney= users[0].kidney;
     const numberofKidneys=johnKidney.length;
-    const numberofHealthyKidneys=0;
-    for(let i=0;i<johnKidney;i++){
+    let numberofHealthyKidneys=0;
+    for(let i=0;i<johnKidney.length;i++){
         if(johnKidney[i].healthy){
             numberofHealthyKidneys=numberofHealthyKidneys+1;
         }
@@ -24,5 +24,38 @@ app.get("/",function(req,res){
         numberofUnhealthyKidneys
     })
      
+})
+
+app.post("/",function(req,res){
+    const isHealthy= req.body.isHealthy;
+    users[0].kidney.push({
+        healthy:isHealthy  
+    })
+    res.json({
+        msg:"Done!"
+    })
+
+})
+
+app.put("/",function(req,res){
+    for(let i=0;i<users[0].kidney.length;i++){
+        users[0].kidney[i].healthy=true;
+    }
+    res.json({
+        msg:"put done!"
+    })
+})
+
+app.delete("/",function(req,res){
+    const newkidney=[];
+    for(let i=0;i<users[0].kidney.length;i++){
+        if(users[0].kidney[i].healthy){
+            newkidney.push({
+                healthy:true
+            })
+        }
+    }
+    users[0].kidney=newkidney;
+    res.json({msg:"done!"})
 })
 app.listen(3000)

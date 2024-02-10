5d723208ba117ddc32478aae8ade5c1a965be127
jainam-b <jainambagrecha16@gmail.com>
2024-02-10 21:42:02 +0530

    completed

--- React/cohort-react/src/App.jsx
+++ React/cohort-react/src/App.jsx
@@ -1,18 +1,30 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
 import { useState } from "react";
 import "./App.css";
 
  
 function App() {
+  
   const [count,setcount]=useState(0);
-  function onclickHandler(){
-    setcount(count+1)
-  }
   return (
     <div>
-      <button onClick={onclickHandler}>Counter {count}</button>
+       <CustomButton count={count} setcount={setcount} ></CustomButton>
     </div>
   );
 }
+
+function CustomButton(props) {
+  function onclickHandler() {
+    props.setcount(props.count + 1);
+  }
+
+  return (
+    <button onClick={onclickHandler}>
+      Counter {props.count}
+    </button>
+  );
+}
+
 
 export default App;

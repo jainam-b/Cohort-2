ee0cebf6546c3bcc50dc537599887d1d3ffb3233
jainam-b <jainambagrecha16@gmail.com>
2024-02-10 18:45:57 +0530

    counter app completed

--- React/cohort-react/src/App.jsx
+++ React/cohort-react/src/App.jsx
@@ -1,14 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
 
+ 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [count,setcount]=useState(0);
+  function onclickHandler(){
+    setcount(count+1)
+  }
   return (
     <div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <button onClick={onclickHandler}>Counter {count}</button>
     </div>
   );
 }

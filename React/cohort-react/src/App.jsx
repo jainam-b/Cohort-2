cbca2e4d7751a6953fd361040b771bba64649a87
jainam-b <jainambagrecha16@gmail.com>
2024-02-17 09:39:56 +0530

    todo app in react completed

--- React/cohort-react/src/App.jsx
+++ React/cohort-react/src/App.jsx
@@ -1,35 +1,54 @@
+// Lets create a simple todo app that renders 3 todos
+// Create a Todo component that accepts title, description as input
+// Initialise a state array that has 3 todos
+// Iterate over the array to render all the TODOs
+// A button in the top level App component to add a new TODO
+
 import React from "react";
-import { Fragment } from "react";
+// import { Fragment } from "react";
 import { useState } from "react";
-
-
-function App(){
-  return <div>
-    <HeaderwithButton></HeaderwithButton>
-     
-    <Header title="jainam"></Header>
-       
-
-  </div>
-}
-function HeaderwithButton() {
-  const [title, setTitle] = useState("jainam");
-  function updateTitle(){
-    setTitle("my name is"+ Math.random())
+let counter=4;
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "go to gym",
+      description: "at 6 pm",
+    },
+    {
+      id: 2,
+      title: "go to mess",
+      description: "at 8 pm",
+    },
+    {
+      id: 3,
+      title: "go to bed",
+      description: "at 10 pm",
+    },
+  ]);
+  function addTodo(){
+    setTodos([...todos,{
+      id:counter++,
+      title:Math.random(),
+      description:Math.random()
+    }])
   }
   return (
     <div>
-      <button onClick={updateTitle}>click me to change the title</button>
-      <Header title={title}></Header>
+      <button onClick={addTodo}>Add a new todo</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} description={todo.description} />
+      ))}
     </div>
   );
 }
-
-// for not re rendering we use useMemo
-const Header=React.memo(function Header({title}){
-  return <div>
-    {title}
-  </div>
-})
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h3>{title} </h3>
+      <p>{description} </p>
+    </div>
+  );
+}
 
 export default App;

3e3edb7d58cf58f95136147af889f741e3ab942a
jainam-b <jainambagrecha16@gmail.com>
2024-02-21 19:42:14 +0530

    retiving todo with button click

--- hooks-react/hook/src/App.jsx
+++ hooks-react/hook/src/App.jsx
@@ -3,28 +3,61 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  
+  const [selectedid, setSelectedtid] = useState(1);
 
-  return <>
-  {/* {todos.map(todo =><Todos title={todo.title}  description={todo.description}> </Todos>)} */}
-  <Todos id={1}></Todos>
-  </>;
+   
+  return (
+    <>
+      <button
+        onClick={function () {
+          setSelectedtid(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={function () {
+          setSelectedtid(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={function () {
+          setSelectedtid(3);
+        }}
+      >
+        3
+      </button>
+      <button
+        onClick={function () {
+          setSelectedtid(4);
+        }}
+      >
+        4
+      </button>
+      <Todos id={selectedid}></Todos>
+    </>
+  );
 }
 
-function Todos({id}){
-  
+function Todos({ id }) {
   const [todo, setTodos] = useState([]);
-  
+
   useEffect(() => {
     axios
-      .get("https://sum-server.100xdevs.com/todo?id="+id )
+      .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
       .then(function (response) {
+        console.log(response);
         setTodos(response.data.todo);
       });
-  }, []);
-  return <div>
-    <h1>{ todo.title}</h1>
-    <h1>{ todo.description}</h1>
-  </div>
+  }, [id]);
+
+  return (
+    <div>
+      <h1>{todo.title}</h1>
+      <h1>{todo.description}</h1>
+    </div>
+  );
 }
 export default App;

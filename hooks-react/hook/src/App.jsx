5b104f37c4d583515995fb84e5d427893954ab14
jainam-b <jainambagrecha16@gmail.com>
2024-02-21 21:52:35 +0530

    useMemo

--- hooks-react/hook/src/App.jsx
+++ hooks-react/hook/src/App.jsx
@@ -3,61 +3,24 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [selectedid, setSelectedtid] = useState(1);
+  let [count, setcount] = useState(0);
+  const [inputValue, setInputValue] = useState(1)
+  let counts=useMemo(() => {
+    let counts=0
+    for(let i=1;i<=inputValue;i++){
+      counts=counts+i
+    }
+  }, [inputValue])
 
-   
   return (
     <>
-      <button
-        onClick={function () {
-          setSelectedtid(1);
-        }}
-      >
-        1
-      </button>
-      <button
-        onClick={function () {
-          setSelectedtid(2);
-        }}
-      >
-        2
-      </button>
-      <button
-        onClick={function () {
-          setSelectedtid(3);
-        }}
-      >
-        3
-      </button>
-      <button
-        onClick={function () {
-          setSelectedtid(4);
-        }}
-      >
-        4
-      </button>
-      <Todos id={selectedid}></Todos>
+      <input type="number" onChange={function(e){
+        setInputValue(e.target.value)
+      }} />
+      <h2>Sum is {counts}</h2>
+      <button onClick={() => setcount(count+1)}>Count ({count}) </button>
     </>
   );
 }
 
-function Todos({ id }) {
-  const [todo, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
-      .then(function (response) {
-        console.log(response);
-        setTodos(response.data.todo);
-      });
-  }, [id]);
-
-  return (
-    <div>
-      <h1>{todo.title}</h1>
-      <h1>{todo.description}</h1>
-    </div>
-  );
-}
 export default App;

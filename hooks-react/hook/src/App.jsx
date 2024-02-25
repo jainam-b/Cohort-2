f0cd91ad3a2c0ab1a3eb4fa3e88b4f4a9affb971
jainam-b <jainambagrecha16@gmail.com>
2024-02-25 09:40:11 +0530

    Routing with lazy loading

--- hooks-react/hook/src/App.jsx
+++ hooks-react/hook/src/App.jsx
@@ -1,6 +1,7 @@
+import React ,{Suspense}  from "react";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import { Dashboard } from "./components/Dashboard";
-import { Landing } from "./components/Landing";
+const  Dashboard = React.lazy(()=> import("./components/Dashboard")); 
+const  Landing = React.lazy(()=> import("./components/Landing")); 
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <BrowserRouter>
         <Appbar></Appbar>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/" element={<Landing />}></Route>
+          <Route path="/dashboard" element={ <Suspense fallback={"loading ..."}> <Dashboard /></Suspense>}></Route>
+          <Route path="/" element={ <Suspense fallback={"loading ..."}> <Landing /></Suspense>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
